53e2fc15c9db35c726b01f884587e8c6b7858ed7
Alexander Wingård <alexander.wingard@gmail.com>
2011-08-21 01:18:50 +0200

    Initialize the matrix with borders

--- game.js
+++ game.js
@@ -1,6 +1,11 @@
 var c = document.getElementById("theCanvas")
 var cx = c.getContext("2d");
 
+var left = 1;
+var right = 2;
+var up = 4;
+var down = 8;
+
 function init() {
     var rows = 10;
     var cols = 10;
@@ -9,8 +14,18 @@ function init() {
     for(var r = 0; r < rows ; r++) {
 	matrix.push([]);
 	for(var c = 0; c < cols; c++) {
-	    matrix[r].push(0);
+	    var cell = 0;
+	    if (r == 0)
+		cell = cell | down;
+	    if (r == rows - 1)
+		cell = cell | up;
+	    if (c == 0)
+		cell = cell | left;
+	    if (c == cols - 1)
+		cell = cell | right;
+	    matrix[r].push(cell);
 	}
+	console.log(matrix[matrix.length -1 ]);
     }
     alert(matrix);
 }
