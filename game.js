bc8df26843a34fa181f24861524ac47a1b708d50
Alexander Wingård <alexander.wingard@gmail.com>
2011-08-22 00:32:28 +0200

    Begin of makepath, we can make a hole

--- game.js
+++ game.js
@@ -6,20 +6,22 @@ var right = 2;
 var up = 4;
 var down = 8;
 
+var inaccess = left | right | up | down;
+
 var w = c.width;
 var h = c.height;
 
 var rows = 10;
 var cols = 10;
 
+var matrix = [];
 
 function init() {
-    var matrix = [];
-    
     for(var r = 0; r < rows ; r++) {
 	matrix.push([]);
 	for(var c = 0; c < cols; c++) {
-	    var cell = 0;
+	    var cell = up | down | left | right;
+	    /*
 	    if (r == 0)
 		cell = cell | up;
 	    if (r == rows - 1)
@@ -28,13 +30,33 @@ function init() {
 		cell = cell | left;
 	    if (c == cols - 1)
 		cell = cell | right;
+	    */
 
 	    matrix[r].push(cell);
-
-	    drawCell(c, r, cell);
 	}
     }
+    makePath(5,5);
+    
+    for(var x = 0; x < matrix.length; x++) {
+	for(var y = 0; y < matrix[x].length; y++) {
+	    drawCell(y, x, matrix[x][y]);
+	}
+	console.log(matrix[x]);
+    }
 }
+
+function makePath(x,y) {
+    var neighbours = [];
+    
+    if(x > 1 && matrix[x-1][y] == inaccess) {
+	neighbours.push(matrix[x-1][y]);
+	matrix[x][y-1] = inaccess & ~right;
+	matrix[x][y] = inaccess & ~left;
+    }
+    
+    console.log(neighbours);
+}
+
 
 function drawCell(x, y, status) {
     var part = h / rows;
