6c7f2a4690f88ca6f147a2ead50a41da90b5ad9e
Alexander Wingård <alexander.wingard@gmail.com>
2011-08-24 00:35:26 +0200

    Fix my totally incorrect interpretation of which axis is which

--- game.js
+++ game.js
@@ -1,92 +1,93 @@
 var c = document.getElementById("theCanvas");
 var cx = c.getContext("2d");
 
-var left = 1;
-var right = 2;
-var up = 4;
-var down = 8;
+var left    = 1;
+var right   = 2;
+var up      = 4;
+var down    = 8;
 var visited = 16;
-
-var inaccess = left | right | up | down;
 
 var w = c.width;
 var h = c.height;
 
-var rows = 10;
-var cols = 10;
+var rows = 20;
+var cols = 20;
+
+var wpx = w / cols;
+var hpx = h / rows;
 
 var matrix = [];
 
 function init() {
-    for(var r = 0; r < rows ; r++) {
+    for(var x = 0; x < cols; x++) {
 	matrix.push([]);
-	for(var c = 0; c < cols; c++) {
+	for(var y = 0; y < rows; y++) {
 	    var cell = up | down | left | right;
-	    matrix[r].push(cell);
+	    matrix[x].push(cell);
 	}
     }
-    makePath(5,5);
-    
-    for(var x = 0; x < matrix.length; x++) {
-	for(var y = 0; y < matrix[x].length; y++) {
-	    drawCell(y, x, matrix[x][y]);
-	}
-    }
+    makePath(0,0);
+    drawMaze();
 }
 
 function makePath(x,y) {
     matrix[x][y] |= visited;
 
     var upf = function() {
-	if(x > 0 && (matrix[x-1][y] & visited) == 0) {
+	if(y > 0 && (matrix[x][y-1] & visited) == 0) {
 	    matrix[x][y] &= ~up;
-	    matrix[x-1][y] &= ~down;
-	    makePath(x-1, y)
+	    matrix[x][y-1] &= ~down;
+	    makePath(x, y-1)
 	}
     }
     var downf = function() {
-	if(x < rows-1 && (matrix[x+1][y] & visited) == 0) {
+	if(y < rows-1 && (matrix[x][y+1] & visited) == 0) {
 	    matrix[x][y] &= ~down;
-	    matrix[x+1][y] &= ~up;
-	    makePath(x+1, y)
+	    matrix[x][y+1] &= ~up;
+	    makePath(x, y+1)
 	}
     }
     var rightf= function() {
-	if(y < cols-1 && (matrix[x][y+1] & visited) == 0) {
+	if(x < cols-1 && (matrix[x+1][y] & visited) == 0) {
 	    matrix[x][y] &= ~right;
-	    matrix[x][y+1] &= ~left;
-	    makePath(x, y+1);
+	    matrix[x+1][y] &= ~left;
+	    makePath(x+1, y);
 	}
     }
     var leftf= function() {
-	if(y > 0 && (matrix[x][y-1] & visited) == 0) {
+	if(x > 0 && (matrix[x-1][y] & visited) == 0) {
 	    matrix[x][y] &= ~left;
-	    matrix[x][y-1] &= ~right;
-	    makePath(x, y-1);
+	    matrix[x-1][y] &= ~right;
+	    makePath(x-1, y);
 	}
     }
-    
+  
     var dirs = [upf, rightf, downf, leftf];
     dirs.sort(function() { return Math.round(Math.random()) - 0.5 });
     for(var i = 0; i < dirs.length; i++) {
-	if(dirs[i]())
-	    return true;
+	dirs[i]()
     }
-    return true;
 }
 
 
+function drawMaze() {
+    for(var x = 0; x < cols; x++) {
+	for(var y = 0; y < rows; y++) {
+	    drawCell(x, y, matrix[x][y]);
+	}
+    }
+}
+
 function drawCell(x, y, status) {
-    var part = h / rows;
     function maybeDraw(x, y, maybe) {
 	maybe ? cx.lineTo(x, y) : cx.moveTo(x, y);
     }
     cx.beginPath();
-    maybeDraw(x * part, y * part, false);
-    maybeDraw(x * part + part, y * part, status & up);
-    maybeDraw(x * part + part, y * part + part, status & right);
-    maybeDraw(x * part, y * part + part, status & down);
-    maybeDraw(x * part, y * part, status & left);
+    maybeDraw(x * wpx, y * hpx, false);
+    maybeDraw(x * wpx + wpx, y * hpx, status & up);
+    maybeDraw(x * wpx + wpx, y * hpx + hpx, status & right);
+    maybeDraw(x * wpx, y * hpx + hpx, status & down);
+    maybeDraw(x * wpx, y * hpx, status & left);
     cx.stroke();
 }
 
